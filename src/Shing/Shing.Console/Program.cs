a8dd44b5579f656b483699051c13da9847aabe74
John Bubriski <bubriski@gmail.com>
2012-03-16 13:33:03 -0400

    Updated the console app to hit the regular view of the wishlist. Definitely issues with it, but it does find products, but some aren't actually in the wishlist, i.e. product suggestions.

--- src/Shing/Shing.Console/Program.cs
+++ src/Shing/Shing.Console/Program.cs
@@ -17,14 +17,29 @@
 
             var scraper = new CompactScraper();
 
-            var productIds = scraper.Scrape(wishlistUrlCompact);
+            var productIds = scraper.Scrape(wishlistUrl);
 
+            Console.WriteLine();
+            Console.WriteLine("Regular");
+            Console.WriteLine();
+            PrintResults(amazonBaseShortLink, productIds);
+
+            productIds = scraper.Scrape(wishlistUrlCompact);
+
+            Console.WriteLine();
+            Console.WriteLine("Compact");
+            Console.WriteLine();
+            PrintResults(amazonBaseShortLink, productIds);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintResults(string amazonBaseShortLink, List<string> productIds)
+        {
             foreach (var productId in productIds)
             {
                 Console.WriteLine(string.Format("{0}:\t{1}{0}", productId, amazonBaseShortLink));
             }
-
-            Console.ReadKey();
         }
     }
 }
