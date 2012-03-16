97c934372d80aebb152a6b3e7f4689ed3642143c
Jared Barboza <jared.m.barboza@gmail.com>
2012-03-16 13:54:14 -0400

    First swag at adding a bit of OO to the project

---
+++ src/Shing/Shing/Models/WishListItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shing.Models
+{
+    public class WishListItem : Contracts.WishListItem
+    {
+        public string FriendlyName
+        {
+            get;
+            set;
+        }
+
+        public string ProductId
+        {
+            get;
+            set;
+        }
+
+        public decimal Price
+        {
+            get;
+            set;
+        }
+
+        public Uri MoreInfoUrl
+        {
+            get;
+            set;
+        }
+    }
+}
